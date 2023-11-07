b1d4b12d20d2f2436efd97f30a00c5ba97465d1d
Mateescu Denis <denis.mateescu01@gmail.com>
2023-11-07 22:33:58 +0200

    Initial commit

---
+++ e-commerce-frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './Global CSS/App.css';
+import { Button } from 'react-bootstrap';
+import Header from './Global Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login & Register/Login';
+import Register from './Login & Register/Register';
+import AddProduct from './Product Component/AddProduct';
+import UpdateProduct from './Product Component/UpdateProduct';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <h1>BuyMe</h1>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/add" element={<AddProduct />} />
+          <Route path="/update" element={<UpdateProduct />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
